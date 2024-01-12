6c194556df3b404fcf169926a8d3dcc103a8ab72
HenriqueBispo <henriquesouzabispo848@gmail.com>
2024-01-12 15:00:16 -0300

    f

--- server/index.js
+++ server/index.js
@@ -1,28 +1,27 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, { cors: { origin: 'http://localhost:5173' } });
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
 
-const HOST = '192.168.0.102';
-const PORT = 3001;
+const PORT = 3001
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('Usuário conectado!', socket.id);
 
-  socket.on('disconnect', (reason) => {
-    console.log('Usuário desconectado!', socket.id);
-  });
+  socket.on('disconnect', reason => {
+    console.log('Usuário desconectado!', socket.id)
+  })
 
-  socket.on('set_username', (username) => {
-    socket.data.username = username;
-  });
+  socket.on('set_username', username => {
+    socket.data.username = username
+  })
 
-  socket.on('message', (text) => {
+  socket.on('message', text => {
     io.emit('receive_message', {
       text,
       authorId: socket.id,
-      author: socket.data.username,
-    });
-  });
-});
+      author: socket.data.username
+    })
+  })
+})
 
-server.listen(PORT, HOST, () => console.log(`Server running at http://${HOST}:${PORT}`));
+server.listen(PORT, () => console.log('Server running...'))
